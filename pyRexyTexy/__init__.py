a35ce1e3bb60b55bbb5a5778447a0ec0e2e5c3c5
sapbotgit <120243584+sapbotgit@users.noreply.github.com>
2023-11-22 12:15:43 +0500

    Update __init__.py

--- pyRexyTexy/__init__.py
+++ pyRexyTexy/__init__.py
@@ -1,10 +1,13 @@
-import serial
+import serial, sys
 
 class Screen:
   def __init__(self, port="COM1"):
-    self.ser = serial.Serial(port, 9600)
+    try:
+      self.ser = serial.Serial(port, 9600)
+    except:
+      self.ser = sys.stdout
   def draw(x:int, y:int, state:bool):
     st = {True:1, False:0}
-    self.ser.write(bytes(f"XY {x} {y} {st[state]}"))
+    self.ser.write(bytes(f"XY {x} {y} {st[state]};"))
   def text(txt="Hello World"):
-    self.ser.write(bytes(f"TEXT {txt}"))
+    self.ser.write(bytes(f"TEXT {txt};"))
